0921d71621e7fec8e8368bff515c49d1e2a47663
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-12-07 22:23:34 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,10 +32,16 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n=== TEST 4: seller Insert ===");
+        System.out.println("\n=== TEST 4: seller insert ===");
         Seller seller1 = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
         sellerDao.insert(seller);
         System.out.println("Inserted! New id = " + seller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller1 = sellerDao.findById(1);
+        seller1.setName("Martha Wayne");
+        sellerDao.update(seller1);
+        System.out.println("Update completed");
 
     }
 
